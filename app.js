70bef60d261b400ed53912ae24a6f585251a50d3
MammadovAli238 <memmedoveli1223@gmail.com>
2024-10-27 11:42:15 +0400

    homework

---
+++ app.js
@@ -0,0 +1,26 @@
+//task1 
+
+let firstName = "Ali";
+let lastName = "Mammadov";
+let fullName = firstName + " " + lastName;
+console.log("Sizi bir daha görməyimizə şadıq," + " " + fullName);
+
+
+//task2 
+
+fullName = "Yusif Zeynalov";
+console.log(fullName);
+console.log(fullName.length - 1);
+
+//task3 (fail)
+fullName = "Cavid" + " " + "Cabbarov"
+console.log(fullName[0] + fullName[6]);
+
+
+
+
+//task4 
+firstName = "Solmaz";
+lastName = "Kerimova";
+fullName = lastName + " " + firstName;
+console.log(fullName);
